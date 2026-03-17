fb44af62439b042d72d12f8cd036b2ac313b4e50
BDSturgeon <Bdsmadlu@gmail.com>
2026-03-17 12:42:45 -0500

    I added more telemetry to see if the code is taking joystick inputs.(It is taking inputs, but it is not moving)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpBase.java
@@ -153,8 +153,12 @@ public abstract class TeleOpBase extends OpMode {
                 //drive.drive(-gamepad1.left_stick_y * speedMultiplier, gamepad1.left_stick_x * speedMultiplier, gamepad1.right_stick_x * speedMultiplier);
             if(followerInitialized) {
                 follower.setTeleOpDrive(-gamepad1.left_stick_y * speedMultiplier, -gamepad1.left_stick_x * speedMultiplier, -gamepad1.right_stick_x * speedMultiplier, false);
+                telemetry.addLine("FC Right Stick X,Y" + gamepad1.left_stick_x + ", " + gamepad1.left_stick_y);
+                telemetry.addLine("FC Left Stick X" + gamepad1.right_stick_y);
             } else{
                 drive.drive(-gamepad1.left_stick_y * speedMultiplier, -gamepad1.left_stick_x * speedMultiplier, -gamepad1.right_stick_x * speedMultiplier);
+                telemetry.addLine("RC Right Stick X,Y" + gamepad1.left_stick_x + ", " + gamepad1.left_stick_y);
+                telemetry.addLine("RC Left Stick X" + gamepad1.right_stick_y);
             }
         }
 
