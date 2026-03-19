430c21beec6bdb4ce0a799f516e77d7882746af9
BDSturgeon <Bdsmadlu@gmail.com>
2026-03-19 09:28:51 -0500

    Nevermind, I changed one number wrong

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCloseTwelveBall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCloseTwelveBall.java
@@ -36,7 +36,7 @@ public class BlueCloseTwelveBall extends OpMode {
     private final Pose pickup1Pose = new Pose(24, 63, Math.toRadians(180)); // Pick up closest row of balls
     private final Pose openGateReadyPose = new Pose(30, 66, Math.toRadians(90)); // Ready to open gate
     private final Pose openGatePose = new Pose(18, 66, Math.toRadians(90)); // Open gate
-    private final Pose pickupReady2Pose = new Pose(51.5, 87, Math.toRadians(180)); //Ready to pick up middle row of balls
+    private final Pose pickupReady2Pose = new Pose(48.5, 87, Math.toRadians(180)); //Ready to pick up middle row of balls
     private final Pose pickup2Pose = new Pose(24, 87, Math.toRadians(180)); //Pick up middle row of balls
     private final Pose pickupReady3 = new Pose(50, 40, Math.toRadians(180)); //Ready to pick up far balls
     private final Pose pickup3Pose = new Pose(24, 40, Math.toRadians(180)); //Finish with 3 balls
